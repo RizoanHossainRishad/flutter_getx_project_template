4d1538eb9d6df2f85c5c81e150cefce1ff9a284f
Rizoan Hossain Rishad <95619717+RizoanHossainRishad@users.noreply.github.com>
2026-02-07 09:43:58 +0600

    custom logger
    my very own created custom logger

--- lib/utils/app_log.dart
+++ lib/utils/app_log.dart
@@ -3,6 +3,45 @@ import 'package:flutter/foundation.dart';
 
 import 'error_log.dart';
 
+import 'dart:developer';
+import 'package:flutter/foundation.dart';
+
+import 'error_log.dart';
+
+
+
+
+
+//Better alternative
+Future<void> appLogger({
+  required String title,
+  required dynamic message,
+
+}) async {
+
+  if (kDebugMode) {
+    log(
+      "🪘🪘========================================= $title ======================================🪘🪘",
+    );
+    log(message.toString());
+    log(
+      "🪘🪘====================================End============================================🪘🪘",
+    );
+  }
+}
+
+
+void errorLogger(String message, dynamic e, {String title = "Error form"}) {
+  try {
+    if (kDebugMode) {
+      log("🎌🎌🪖🎴🎴🎴🎴🎴🎴🪖🎌🎌 $title > $message >>> 🧐🧐 ${e.toString()} 👏👏👏👏👏👏👏👏👏👏👏👏👏👏👏👏👏");
+    }
+  } catch (e) {
+    ///////
+  }
+}
+
+//Simple approach
 void appLog(dynamic message) {
   try {
     if (kDebugMode) {
@@ -19,3 +58,50 @@ $message
     errorLog("app log", e);
   }
 }
+
+
+
+class AppLogColor {
+  static const _reset = '\x1B[0m';
+  static const _red = '\x1B[31m';
+  static const _green = '\x1B[32m';
+  static const _yellow = '\x1B[33m';
+  static const _blue = '\x1B[34m';
+  static const _cyan = '\x1B[36m';
+
+  static void info(String message) {
+    if (kDebugMode) {
+      debugPrint('$_blue[INFO] $message$_reset');
+
+    }
+  }
+
+  static void success(String message) {
+    if (kDebugMode) {
+      debugPrint('$_green[SUCCESS] $message$_reset');
+
+    }
+  }
+
+  static void warning(String message) {
+    if (kDebugMode) {
+      debugPrint('$_yellow[WARNING] $message$_reset');
+    }
+  }
+
+  static void error(String message, dynamic e, {String title = "Error form"}) {
+    if (kDebugMode) {
+      debugPrint('$_red[ERROR] $title  $message → ${e.toString()}$_reset');
+
+    }
+  }
+
+  static void debug(String message) {
+    if (kDebugMode) {
+      debugPrint('$_cyan[DEBUG] $message$_reset');
+    }
+  }
+}
+
+
+
